9be5343205a6b2619546eee536ab8fcc5cf359c7
mc <mcfangpy@gmail.com>
2023-12-28 22:29:28 +0800

    fix: fix cache key

--- pages/api/image/cache.tsx
+++ pages/api/image/cache.tsx
@@ -1,26 +1,39 @@
 import { kv } from '@vercel/kv';
 
 
+export const config = {
+  runtime: "edge",
+};
+
 export default async function GET(req: any, res: any) {
-  if (!req.query || !Object.keys(req.query)) {
-    return res.status(500).json({ error: 'lack params' })
+  const data = (req.nextUrl as any).search.slice(1) || "";
+  const params = data.split('&')
+  let key = ''
+  for (var i = 0; i < params.length; i++) {
+    var pair = params[i].split("=");
+    key = key + ':' + pair[1]
   }
-  const key = req.query.addr + ':' + req.query.exchange
-  if (key) {
+  if (key.length > 0) {
     console.log(`key--- ${key}`)
     try {
       const kvValue = await kv.get(key);
       console.log(`kvValue--- ${kvValue}`)
       const imageUrl = `https://node.tradao.xyz/api/user/generate?${kvValue}`
       // res.statusCode = 200;
-      res.status(200).json({ imageUrl });
+      Response.json({ imageUrl }, {
+        status: 200
+      });
     } catch (e) {
       // res.statusCode = 500;
       console.log("kv get err", e);
-      res.status(500).json({ error: "Internal Server Error" });
+      Response.json({ error: "Internal Server Error" }, {
+        status: 500
+      });
     }
   } else {
     // res.statusCode = 400;
-    res.status(200).json({ error: "Bad Request" });
+    Response.json({ error: "Bad Request" }, {
+      status: 200
+    });
   }
 }
